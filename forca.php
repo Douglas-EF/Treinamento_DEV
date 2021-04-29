c94d2bb9959dcbba9f8a7007147b2f55746d5e33
Douglas-EF <douglas@eficienciafiscal.com.br>
2021-04-29 19:07:36 -0400

    Inserindo código funcial do game

--- forca.php
+++ forca.php
@@ -1,50 +1,140 @@
+import os;
+
 <?php
 
+$ary_palavra;
+$ary_tracejada;
+$ary_verificada;
 
-// Contando a quantidade de palavras para selecionar com base no indice
-$consulta = consultarPalavras();
-
-$numero_random = random_int(0, array_key_last($dados));
-echo "----------------------------\n\n";
-echo $numero_random;
-searchPalavra($consulta, $numero_random);
-echo "\n\n ---------------------------";
-/*
-animacao_0();
-animacao_1();
-animacao_2();
-animacao_3();
-animacao_4();
-animacao_5();
-animacao_6();
-animacao_7();
-animacao_8();
-animacao_9();
-animacao_10();
-*/
-//print_r($qtd_palavras."\n\n\n");
-//echo count($dados);
-//print_r($numero_random[1]);
+$plv_tracejada;
+$list_keys = [];
+$life = 0;
 
 
 
+$opcoes = array(1, 2, 3);
+
+while (true) {
+    $opc_selected = exibeMenu();
+    if ($opc_selected == 1) {
+        // Contando a quantidade de palavras para selecionar com base no indice
+        $consulta = consultarPalavras();
+
+        $numero_random = random_int(0, array_key_last($dados));
+        echo "----------------------------\n\n";
+        //echo $numero_random;
+
+        $palavra = searchPalavra($numero_random);
+        //converterDados($palavra);
+        $plv_tracejada = tracejarPalavra($palavra);
+        animacao(0);
+        converterDados($palavra);
+
+        while (true) {
+            echo "┗━━━━━> " . $plv_tracejada . "\n\n";
+            //converterDados($palavra);
+
+            system("cls");
+            $letra_informada = readline("Informe uma letra:" . "\n");
+
+            if (in_array($letra_informada, $ary_palavra)) {
+
+                // Enquanto tiver keys corespondentes a determinada letra ele ira prencher o array($ary_tracejada)
+                $list_keys = array_search_all($letra_informada, $ary_palavra);
+                foreach ($list_keys as $key) {
+                    $ary_tracejada[$key] = "$letra_informada";
+                }
+
+                $plv_tracejada = implode($ary_tracejada);
+                print_r($ary_tracejada);
+            } else {
+                $life++;
+                animacao($life);
+
+                if ($life > 7) {
+                    echo "Game Over...";
+                    return false;
+                    system('ls');
+                }
+            }
+        }
+    } elseif ($opc_selected == 2) {
+        echo "Opção 2!";
+    } elseif ($opc_selected == 3) {
+        return false;
+    }
+
+    //palavra($palavra);
+    $exibe_palavra = " ";
+    //$tracejada = mb_str_pad($exibe_palavra, strlen($palavra),"-" );
+    echo strlen($palavra);
+}
 
 
 
+//
+function mb_str_pad($input, $pad_length, $pad_string = ' ', $pad_type = STR_PAD_RIGHT, $encoding = 'UTF-8')
+{
+    $mb_diff = mb_strlen($input, $encoding) - strlen($input);
+    return str_pad($input, $pad_length - $mb_diff, $pad_string, $pad_type);
+}
 
+// Função para exibir o menu
+function exibeMenu()
+{
+    global $opcoes;
+    echo "
+    \n" . "┗━━━━━━━━━━━━━━━| GAME FORCA MENU |━━━━━━━━━━━━━━━┛" . "\n \n
+             ┏━━━━━━━━━━━━━━━━━━━━━━━┓
+             | 1 - Iniciar           | 
+             | 2 - Cadastrar palavra |
+             | 3 - Sair              |
+             ┗━━━━━━━━━━━━━━━━━━━━━━━┛     
+    " . "\n\n";
 
+    $opc_selected = (int) readline("Escolha uma opção: ");
 
+    if (!in_array($opc_selected, $opcoes)) {
+        echo "Opção invalida!";
+        return exibeMenu();
+    }
 
+    return $opc_selected;
+}
 
+// Função para percorrer todo o Array em busca de uma determinada letra, e retonar mais de uma chave(caso tenha)
+function array_search_all($letra, $array)
+{
+    foreach ($array as $keys => $valor) {
+        if ($array[$keys] == $letra) {
+            $list[] = $keys;
+        }
+    }
+    return ($list);
+}
 
+// Função
+function converterDados($palavra)
+{
+    global $ary_palavra, $ary_tracejada;
 
+    $plv_tracejada = mb_str_pad("", strlen($palavra), "-");
+    //echo $plv_tracejada . "\n\n";
 
+    $ary_palavra = str_split($palavra);
+    //print_r($ary_palavra);
 
+    $ary_tracejada = str_split($plv_tracejada);
+        //print_r($ary_tracejada);
 
+    ;
+}
 
-
-
-
+function tracejarPalavra($palavra)
+{
+    $plv_tracejada = mb_str_pad("", strlen($palavra), "-");
+    return $plv_tracejada;
+}
 
 // Função para armazenar em array as palavras salvas no arquivo palavras.txt 
 function consultarPalavras()
@@ -65,163 +155,121 @@ function consultarPalavras()
 }
 
 // Função para trazer a palavra que corresponde ao numero gerado
-function searchPalavra($palavra, $numero)
+function searchPalavra($numero)
 {
-    $palavra["$numero"];
-    print_r($palavra);
+    global $dados;
+    $teste = $dados[$numero];
+    //print_r($teste);
+
+    return $teste['palavra'];
 }
 
-// Animações game
-function animacao_0()
+// Animações da forca
+function animacao($case)
 {
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |              
-    |        
-    |       
-    |
-    |
-    |____
-    ";
-}
+    switch ($case) {
+        case 0:
+            echo "
+            ┏━━━━━━━┓
+            |
+            |
+            |
+            |
+            |
+            |
+            |
+            ";
+            break;
 
-function animacao_1()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |       |        
-    |        
-    |       
-    |
-    |
-    |____
-    ";
-}
+        case 1:
+            echo "
+            ┏━━━━━━━┓
+            |       O
+            |              
+            |        
+            |       
+            |
+            |
+            |
+            ";
+            break;
 
-function animacao_2()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |       |\        
-    |        
-    |       
-    |
-    |
-    |____
-    ";
-}
+        case 2:
+            echo "
+            ┏━━━━━━━┓
+            |       O
+            |       |
+            |
+            |
+            |
+            |
+            |
+            ";
+            break;
 
-function animacao_3()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |       |\        
-    |         *
-    |       
-    |
-    |
-    |____
-    ";
-}
+        case 3:
+            echo "
+            ┏━━━━━━━┓
+            |       O
+            |       |\        
+            |
+            |
+            |
+            |
+            |
+            ";
+            break;
 
-function animacao_4()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |      /|\        
-    |         *
-    |       
-    |
-    |
-    |____
-    ";
-}
+        case 4:
+            echo "
+            ┏━━━━━━━┓
+            |       O
+            |      /|\        
+            |         
+            |       
+            |
+            |
+            |
+            ";
+            break;
 
-function animacao_5()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |      /|\        
-    |     *   *
-    |       
-    |
-    |
-    |____
-    ";
-}
+        case 5:
+            echo "
+            ┏━━━━━━━┓
+            |       O
+            |      /|\        
+            |       |  
+            |
+            |
+            |
+            |
+            ";
+            break;
 
-function animacao_6()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |      /|\        
-    |     * | *
-    |       
-    |
-    |
-    |____
-    ";
-}
+        case 6:
+            echo "
+            ┏━━━━━━━┓
+            |       O
+            |      /|\        
+            |       |  
+            |        \
+            |
+            |
+            |
+            ";
+            break;
 
-function animacao_7()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |      /|\        
-    |     * | *
-    |        \
-    |
-    |
-    |____
-    ";
-}
-
-function animacao_8(){
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |      /|\        
-    |     * | *
-    |        \
-    |         *
-    |
-    |____
-    ";
-}
-
-
-function animacao_9()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |      /|\        
-    |     * | *
-    |      / \
-    |         *  
-    |
-    |____
-    ";
-}
-
-function animacao_10()
-{
-    echo "
-    ┏━━━━━━━┓
-    |       O
-    |      /|\        
-    |     * | *
-    |      / \
-    |     *   *  
-    |
-    |____
-    ";
+        case 7:
+            echo "
+            ┏━━━━━━━┓
+            |       O
+            |      /|\        
+            |       |  
+            |      / \
+            |
+            |
+            |
+            ";
+            break;
+    }
 }
